e2496af51a55630db579f7bd8324b864da3b4fcf
Chilla Tenga <chilla@devlift.com>
2018-04-02 10:53:15 -0400

    load bubble images from  pod resource bundle

--- AsyncMessagesViewController.podspec
+++ AsyncMessagesViewController.podspec
@@ -5,15 +5,17 @@ Pod::Spec.new do |spec|
   spec.homepage              = 'https://github.com/nguyenhuy/AsyncMessagesViewController'
   spec.authors               = { 'Huy Nguyen' => 'http://huytnguyen.me' }
   spec.summary               = 'A smooth, responsive and flexible messages UI library for iOS apps.'
-  spec.source                = { :git => 'https://github.com/nguyenhuy/AsyncMessagesViewController.git', :tag => spec.version.to_s }
+  spec.source                = { :path => '.'}
+  spec.source_files          = 'Source/**/*.swift'
   spec.module_name           = 'AsyncMessagesViewController'
   spec.header_dir            = 'AsyncMessagesViewController'
   spec.documentation_url     = 'https://github.com/nguyenhuy/AsyncMessagesViewController'
   spec.platform              = :ios
   spec.ios.deployment_target = '9.0'
   spec.requires_arc          = true  
-  spec.source_files          = 'Source/**/*.swift'
-  spec.ios.resource_bundle   = { spec => 'Source/Assets/AsyncMessagesViewController.xcassets' }
+
+  #spec.ios.resource_bundle   = { spec => 'Source/Assets/AsyncMessagesViewController.xcassets' }
+  spec.resource_bundles = {'AsyncMessagesViewController' => ['Source/Assets/AsyncMessagesViewController.bundle/*']}
   spec.dependency            'Texture', '2.6'
   spec.dependency            'SlackTextViewController', '1.9.6'
 end
--- Source/Controllers/MessageBubbleImageProvider.swift
+++ Source/Controllers/MessageBubbleImageProvider.swift
@@ -24,11 +24,41 @@ private func ==(lhs: MessageProperties, rhs: MessageProperties) -> Bool {
 private let kDefaultIncomingColor = UIColor(red: 239 / 255, green: 237 / 255, blue: 237 / 255, alpha: 1)
 private let kDefaultOutgoingColor = UIColor(red: 17 / 255, green: 107 / 255, blue: 254 / 255, alpha: 1)
 
+private let kBundleName = "AsyncMessagesViewController.bundle"
+private let kTableName = "AsyncMessagesViewController"
+
 public class MessageBubbleImageProvider {
     
     private let outgoingColor: UIColor
     private let incomingColor: UIColor
     private var imageCache = [MessageProperties: UIImage]()
+    
+    static func getBubbleImage(_ imageName: String) -> UIImage{
+        let podBundle = Bundle(for: MessageBubbleImageProvider.self)
+        
+        if let bundleURL = podBundle.resourceURL?.appendingPathComponent("AsyncMessagesViewController.bundle"){
+            if let bundle = Bundle.init(url: bundleURL){
+                guard let imagePath = bundle.path(forResource: imageName, ofType: "png") else {
+                    return UIImage()
+                }
+        
+                guard let image = UIImage(contentsOfFile: imagePath) else {
+                    return UIImage();
+                }
+                
+                return image;
+        
+            }
+            else{
+                print("bundle could not be loaded")
+            }
+        }
+        
+        print("bundle could not be found");
+        
+        return UIImage()
+        
+    }
     
     public init(incomingColor: UIColor = kDefaultIncomingColor, outgoingColor: UIColor = kDefaultOutgoingColor) {
         self.incomingColor = incomingColor
@@ -52,7 +82,11 @@ public class MessageBubbleImageProvider {
     
     private func buildBubbleImage(properties: MessageProperties) -> UIImage {
         let imageName = "bubble" + (properties.isOutgoing ? "_outgoing" : "_incoming") + (properties.hasTail ? "" : "_tailless")
-        let bubble = UIImage(named: imageName)!
+        //let bubble = UIImage(named: imageName)!
+//        guard let bubble = Bundle.asyncImage(withName: imageName, andExtension: "png") else {
+//            return UIImage()
+//        }`
+          let bubble =  MessageBubbleImageProvider.getBubbleImage(imageName);
         
         do {
             var normalBubble = try bubble.imageMaskedWith(color: properties.isOutgoing ? outgoingColor : incomingColor)
@@ -73,3 +107,4 @@ public class MessageBubbleImageProvider {
     }
     
 }
+
