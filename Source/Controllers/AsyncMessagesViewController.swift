2f2d3179319ea181b74c9e063f960f40523da00d
Chilla Tenga <chilla@devlift.com>
2018-04-05 12:29:46 -0400

    allow the project to point the delegate to itself in order to register for on select events. add long press events to the project

--- Source/Controllers/AsyncMessagesViewController.swift
+++ Source/Controllers/AsyncMessagesViewController.swift
@@ -13,16 +13,18 @@ import SlackTextViewController
 open class AsyncMessagesViewController: SLKTextViewController {
 
     public let dataSource: AsyncMessagesCollectionViewDataSource
-    public let delegate: ASCollectionDelegate
-    public let asyncCollectionNode: ASCollectionNode
+    //public let delegate: ASCollectionDelegate
+    open let asyncCollectionNode: ASCollectionNode
+    //public let asyncCollectionView: asyncCol
     
     override open var collectionView: ASCollectionView {
         return scrollView as! ASCollectionView
     }
 
-    public init?(dataSource: AsyncMessagesCollectionViewDataSource, delegate: ASCollectionDelegate) {
+    public init?(dataSource: AsyncMessagesCollectionViewDataSource, delegate: ASCollectionDelegate?) {
         self.dataSource = dataSource
-        self.delegate = delegate
+        //self.delegate = delegate
+        
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = UICollectionViewScrollDirection.vertical
         
@@ -31,11 +33,45 @@ open class AsyncMessagesViewController: SLKTextViewController {
         
         asyncCollectionView.backgroundColor = UIColor.white
         asyncCollectionView.scrollsToTop = true
+        
         asyncCollectionNode.dataSource = dataSource
         asyncCollectionNode.delegate = delegate
+        
+        //
         super.init(scrollView: asyncCollectionView)
         
         isInverted = false
+    }
+    
+    open override func viewDidLoad() {
+        //collectionView.backgroundColor = UIColor.red;
+        super.viewDidLoad();
+        let longPressGesture  = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress))
+        longPressGesture.delaysTouchesBegan = true;
+        
+        self.collectionView.addGestureRecognizer(longPressGesture)
+    }
+    
+    func handleLongPress(gesture : UILongPressGestureRecognizer!){
+        if gesture.state != .began {
+            return
+        }
+        
+        let p = gesture.location(in: self.collectionView)
+        
+        if let indexPath = self.collectionView.indexPathForItem(at: p) {
+            // get the cell at indexPath (the one you long pressed)
+            //_ = self.collectionView.cellForItem(at: indexPath)
+            // do stuff with the cell
+            self.didLongPressItem(indexPath)
+            
+        } else {
+            print("couldn't find index path")
+        }
+    }
+    
+    open func didLongPressItem(_ indexPath: IndexPath){
+        print("found the index path after long press at \(indexPath.row)")
     }
 
     required public init(coder aDecoder: NSCoder) {
