8ed2c2da6ce76e75c4a58b41a3aac6ad67b38b73
Chilla Tenga <chilla@devlift.com>
2018-05-24 12:41:56 -0400

    Update media utils file path

--- Source/Utils/MediaUtils.swift
+++ Source/Utils/MediaUtils.swift
@@ -20,7 +20,7 @@ public class MediaUtils {
                 print("This is the filename of a message: \(fileName)");
             }
         }
-
+        
         
     }
     
@@ -54,7 +54,7 @@ public class MediaUtils {
             }
         }
         
-
+        
     }
     
     public static func downloadImageData(urlString: String, _ response: @escaping (_ success: UIImage) -> Void) {
@@ -71,7 +71,7 @@ public class MediaUtils {
     }
     
     public static func downloadThumbnailWith(videoUrlString: String,  _ response: @escaping (_ success: UIImage) -> Void){
-    
+        
         DispatchQueue.global(qos: .background).async(execute: { () -> Void in
             let asset = AVAsset(url: URL(string: videoUrlString)!)
             let assetImgGenerate = AVAssetImageGenerator(asset: asset)
@@ -85,7 +85,7 @@ public class MediaUtils {
                 print(error)
             }
         })
-    
+        
     }
     
     public static func writeImageToCache(fileName: String, imageToSave: UIImage, searchPathDirectory: FileManager.SearchPathDirectory = .libraryDirectory ) -> Bool{
@@ -94,18 +94,18 @@ public class MediaUtils {
             let libraryDirectory = try fileManager.url(for: searchPathDirectory, in: .userDomainMask, appropriateFor: nil, create: false);
             
             
-                //print("let's try and cache these images");
-                
-                let fileURL = libraryDirectory.appendingPathComponent(fileName)
-                
-                if let imageData = UIImageJPEGRepresentation(imageToSave, 1){
-                    try imageData.write(to: fileURL)
-                    print("image was succesfully saved");
-                    // in the future now, you can just check the cached folder for the image;
-                    //response(_returnedImage);
-                    return true
-                }
-                return false
+            //print("let's try and cache these images");
+            
+            let fileURL = libraryDirectory.appendingPathComponent(fileName)
+            
+            if let imageData = UIImageJPEGRepresentation(imageToSave, 1){
+                try imageData.write(to: fileURL)
+                print("image was succesfully saved");
+                // in the future now, you can just check the cached folder for the image;
+                //response(_returnedImage);
+                return true
+            }
+            return false
         }
         catch {
             print(error)
@@ -114,36 +114,36 @@ public class MediaUtils {
     }
     
     public static func getCachedImage(urlString: String, _ response: @escaping (_ success: UIImage) -> Void){
-            let urlPath = urlString.components(separatedBy: "/")
-            var fileName: String?
-            let fileManager = FileManager.default;
+        let urlPath = urlString.components(separatedBy: "/")
+        var fileName: String?
+        let fileManager = FileManager.default;
+        
+        if !urlPath.isEmpty && !(urlPath.last?.isEmpty)!{
+            fileName = urlPath.last
+            //print("This is the filename of the image in this message: \(fileName!)");
             
-            if !urlPath.isEmpty && !(urlPath.last?.isEmpty)!{
-                fileName = urlPath.last
-                //print("This is the filename of the image in this message: \(fileName!)");
-                
-                let path = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)[0] as String
-                let url = URL(fileURLWithPath: path);
-                let filePath = url.appendingPathComponent(fileName!)
-                if(fileManager.fileExists(atPath: filePath.path)){
-                    let _image = UIImage(contentsOfFile: filePath.path);
-                    if(_image != nil){
-                        response(_image!);
-                        return;
-                    }
+            let path = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)[0] as String
+            let url = URL(fileURLWithPath: path);
+            let filePath = url.appendingPathComponent(fileName!)
+            if(fileManager.fileExists(atPath: filePath.path)){
+                let _image = UIImage(contentsOfFile: filePath.path);
+                if(_image != nil){
+                    response(_image!);
+                    return;
                 }
-                
             }
             
-            
-            
+        }
+        
+        
+        
         MediaUtils.downloadImageData(urlString: urlString, { _returnedImage in
             if(fileName != nil){
                 if(MediaUtils.writeImageToCache(fileName: fileName!, imageToSave: _returnedImage)){
                     response(_returnedImage);
                 }
             }
-                
+            
         })
     }
 }
